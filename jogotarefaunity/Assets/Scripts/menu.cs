38d4cc0c413bcd076045f413cb85f0a19fefcc72
XERIFEGOIABA <salomao.rcs@gmail.com>
2023-07-05 10:49:24 -0300

    weq
    ueu

---
+++ jogotarefaunity/Assets/Scripts/menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour
+{
+    public string nomeCena;
+    public GameObject menuPanel;
+
+   
+    void Start()
+    {
+        menuPanel.SetActive(true);
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(nomeCena);
+    }
+}
