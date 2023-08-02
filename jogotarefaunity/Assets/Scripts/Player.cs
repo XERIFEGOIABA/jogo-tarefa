018140291f66a9d5e1212c8c7a1b094b0b63449b
XERIFEGOIABA <salomao.rcs@gmail.com>
2023-08-02 08:44:58 -0300

    inserindo colisão no player e no chão

--- jogotarefaunity/Assets/Scripts/Player.cs
+++ jogotarefaunity/Assets/Scripts/Player.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //variavel para o rigidBody
+    private Rigidbody2D rig;
+    //vetor da posição inicial do player
+    private Vector3 posInicial;
+    //variavel velocidade
+    public float speed;
+    //variavel forca pulo
+    public float JumpForce;
+    //controlar o pulo
+    public bool isJumping;
+    private Vector3 posInicial;
+    public Animator animator;
+    private bool isMoving;
+    public static float health = 1;
+    public GameObject gameOverPanel;
+
+
+
+  // Start is called before the first frame update
     void Start()
     {
-        
+        rig = GetComponent<RigidBody2D>();
+        posInicial = new Vector3(0, 0, 0);
+        transfom.position = posInicial;
+
     }
 
     // Update is called once per frame
